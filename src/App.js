039850dde68cad11892eeec6e0ae333d6162c456
oppappi <oppappi@NeXT.lan>
2018-11-19 21:59:45 +0200

    Update btn

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoList from './TodoList';
+import './css/App.css';
+import Form from './Form'
 
 class App extends Component {
+
+	constructor(){
+		super()
+		const todos = [
+				{ id:1,
+				title: "Hello React",
+				desc: "Started react.",
+				done: false
+				},
+				{ id:2,
+				title: "Hello, Redux!",
+				desc: "Redux also started",
+				done: false
+				},
+			]
+		this.state = {
+			todos: todos,
+			countTodo: todos.length + 1,
+		}
+	}
+
+	handleSubmit(e) {
+			e.preventDefault();
+			const title = e.target.title.value;
+			const desc = e.target.desc.value;
+			const todos = this.state.todos.slice()
+			const countTodo = this.state.countTodo
+
+			todos.push({
+				id: countTodo,
+				title: title,
+				desc: desc,
+				done: false,
+			});
+
+			this.setState({ todos })
+			this.setState({ countTodo: countTodo + 1})
+
+			e.target.title.value = '';
+			e.target.desc.value = '';
+	}
+
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className="app">
+	  	<h1> TODO... </h1>
+		<Form handleSubmit = {this.handleSubmit.bind(this)} />
+		<TodoList todos = {this.state.todos} />
+     </div>
     );
   }
 }
